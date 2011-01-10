7511da7623e49c8c71393441a103d7968820c761
lahola downloader <padre.amaro@inkvizice.org>
2011-01-10 14:22:50 +0100

    some more download tuning

--- download.sh
+++ download.sh
@@ -11,8 +11,8 @@ function refresh()
 	rm -fr www.henryklahola.nazory.cz
 	wget \
 		--no-verbose --output-file $REPODIR/wget.log \
-		--recursive --level 64 --convert-links \
-		--reject 404.html,jpg,JPG,jpeg,JPEG,mp3,MP3,gif,GIF,png,PNG,rtf,RTF,bmp,BMP,mid,MID,rmi,RMI,tit,TIT \
+		--recursive --level 64 --convert-links --max-redirect 0 \
+		--reject jpg,JPG,jpeg,JPEG,mp3,MP3,gif,GIF,png,PNG,rtf,RTF,bmp,BMP,mid,MID,rmi,RMI,tit,TIT \
 		http://www.henryklahola.nazory.cz/ \
 	|| die "can't download"
 }
@@ -21,6 +21,7 @@ function refresh()
 cd $REPODIR
 
 refresh
+sed -ri 's/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} //' wget.log
 git add wget.log www.henryklahola.nazory.cz
 git commit --quiet --all --message 'automatic update'
-git push origin master >/dev/null || die "unable to push"
+git push origin master >/dev/null 2>&1 || die "unable to push"
